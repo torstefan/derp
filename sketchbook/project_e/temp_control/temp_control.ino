bf278bc0a52adf4e6b42ea10983391b944a67e1a
Tor Stafan Lura <torstefan@gmail.com>
2015-06-19 22:34:53 +0200

    Changed code with input of new variables

--- sketchbook/project_e/temp_control/temp_control.ino
+++ sketchbook/project_e/temp_control/temp_control.ino
@@ -41,7 +41,7 @@ int lastPowerState;
 
 int holdTemp = 0;
 int pwr_switch_pause_addition = 0;
-
+int global_counter_to_ignore_stupid_input_bits = 0;
 
 int menuItemSelected;
 
@@ -147,24 +147,31 @@ void loop() {
 }
 
 void get_new_variables_from_serial(){
-
-  // Looks for bytes stored in a 64 byte serial input buffer
+  int new_holdTemp; // Tells the arduno which temperature to reach.
+  int new_pwr_switch_pause_addition; // The pause time between switching the remote power on off.
+  
   if(Serial.available() >0){
-    int new_holdTemp = Serial.parseInt();
-    int new_pwr_switch_pause_addition = Serial.parseInt();
-    
-    if(new_holdTemp > 0){
+    if(global_counter_to_ignore_stupid_input_bits == 0){
+      new_holdTemp = Serial.parseInt();
+      new_pwr_switch_pause_addition = Serial.parseInt();
+
+      global_counter_to_ignore_stupid_input_bits++; // Not working needs removal.
+      
+      if(new_holdTemp > 0){
         holdTemp = new_holdTemp;
-        Serial.print("New_holdTemp=" + (String)holdTemp +" ");    
-    }
-    
-    if(new_pwr_switch_pause_addition > 0){
-      pwr_switch_pause_addition = new_pwr_switch_pause_addition;
-      Serial.print("New_pwr_pause_addition=" + (String)pwr_switch_pause_addition +" ");
+        Serial.print("New_hold_temp=" + (String)holdTemp +" ");    
+      }
+      
+      if(new_pwr_switch_pause_addition !=0 & (MIN_PWR_SWITCH_PAUSE + new_pwr_switch_pause_addition) >= MIN_PWR_SWITCH_PAUSE){
+        pwr_switch_pause_addition = new_pwr_switch_pause_addition;
+        Serial.println("Recived new_pwr_switch_pause_time=" + (String)new_pwr_switch_pause_addition);
+      }    
+      
     }
 
   }
-
+  global_counter_to_ignore_stupid_input_bits = 0;
+  Serial.print("PSP=" + (String)(MIN_PWR_SWITCH_PAUSE + pwr_switch_pause_addition) + " ");
 }
 
 int get_new_hold_temp(){
